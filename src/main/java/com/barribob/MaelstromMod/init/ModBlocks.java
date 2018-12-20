7f29fb08c6fc44976d7b5978e8b20e558839aa61
miyo6032 <miyo6032@colorado.edu>
2018-12-20 13:07:25 -0700

    Added azure planks and fence

--- src/main/java/com/barribob/MaelstromMod/init/ModBlocks.java
+++ src/main/java/com/barribob/MaelstromMod/init/ModBlocks.java
@@ -8,6 +8,7 @@ import com.barribob.MaelstromMod.blocks.BlockAzureTallGrass;
 import com.barribob.MaelstromMod.blocks.BlockAzureVines;
 import com.barribob.MaelstromMod.blocks.BlockBase;
 import com.barribob.MaelstromMod.blocks.BlockDoubleBrownedGrass;
+import com.barribob.MaelstromMod.blocks.BlockFenceBase;
 import com.barribob.MaelstromMod.blocks.BlockAzureFlower;
 import com.barribob.MaelstromMod.blocks.BlockLeavesBase;
 import com.barribob.MaelstromMod.blocks.BlockLogBase;
@@ -40,4 +41,6 @@ public class ModBlocks
 	public static final Block BLUE_DAISY = new BlockAzureFlower("blue_daisy", Material.PLANTS, 0.5f, 0.5f, SoundType.PLANT);
 	public static final Block RUBY_ORCHID = new BlockAzureFlower("ruby_orchid", Material.PLANTS, 0.5f, 0.5f, SoundType.PLANT);
 	public static final Block DOUBLE_BROWNED_GRASS = new BlockDoubleBrownedGrass("double_browned_grass", Material.PLANTS, 0.5f, 0.5f, SoundType.PLANT);
+	public static final Block AZURE_PLANKS = new BlockBase("azure_planks", Material.WOOD, 1.0f, 10, SoundType.WOOD);
+	public static final Block AZURE_FENCE = new BlockFenceBase("azure_fence", Material.WOOD, 1.0f, 10, SoundType.WOOD);
 }
