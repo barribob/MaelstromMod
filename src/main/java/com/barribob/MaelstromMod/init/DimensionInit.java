38f49835acf4bacd1b7c1ef5e93063e7a86d8de7
miyo6032 <miyo6032@colorado.edu>
2019-01-02 17:07:56 -0700

    Removed buggy fog effect

--- src/main/java/com/barribob/MaelstromMod/init/DimensionInit.java
+++ src/main/java/com/barribob/MaelstromMod/init/DimensionInit.java
@@ -5,13 +5,13 @@ import com.barribob.MaelstromMod.world.dimension.azure_dimension.DimensionAzure;
 import net.minecraft.world.DimensionType;
 import net.minecraftforge.common.DimensionManager;
 
-public class DimensionInit 
+public class DimensionInit
 {
-	public static final int DIMENSION_AZURE_ID = 125;
-	public static final DimensionType AZURE = DimensionType.register("azure", "_azure", DIMENSION_AZURE_ID, DimensionAzure.class, false);
+    public static final int DIMENSION_AZURE_ID = 125;
+    public static final DimensionType AZURE = DimensionType.register("azure", "_azure", DIMENSION_AZURE_ID, DimensionAzure.class, false);
 
-	public static void registerDimensions()
-	{
-		DimensionManager.registerDimension(DIMENSION_AZURE_ID, AZURE);
-	}
+    public static void registerDimensions()
+    {
+	DimensionManager.registerDimension(DIMENSION_AZURE_ID, AZURE);
+    }
 }
--- src/main/java/com/barribob/MaelstromMod/util/handlers/ModEventHandler.java
+++ src/main/java/com/barribob/MaelstromMod/util/handlers/ModEventHandler.java
@@ -4,8 +4,8 @@ import com.barribob.MaelstromMod.init.DimensionInit;
 
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -15,23 +15,24 @@ public class ModEventHandler
 {
     @SideOnly(Side.CLIENT)
     @SubscribeEvent
-    public static void onFogDensityRender(EntityViewRenderEvent.FogDensity event){
-	if(event.getEntity().dimension == DimensionInit.DIMENSION_AZURE_ID)
-	{
-	    int fogStartY = 60;
-	    float maxFog = 0.1f;
-	    float fogDensity = 0.01f;
-	    
-	    /**
-	     * alters fog based on the height of the player
-	     */
-	    if (event.getEntity().posY < fogStartY)
-	    {
-		fogDensity += (float) (fogStartY - event.getEntity().posY) * (maxFog / fogStartY);
-	    } 
-	    
-	    event.setDensity(fogDensity);
-	    event.setCanceled(true);
-	}
+    public static void onFogDensityRender(EntityViewRenderEvent.FogDensity event)
+    {
+//	if (event.getEntity().dimension == DimensionInit.DIMENSION_AZURE_ID)
+//	{
+//	    int fogStartY = 60;
+//	    float maxFog = 0.1f;
+//	    float fogDensity = 0.01f;
+//
+//	    /**
+//	     * alters fog based on the height of the player
+//	     */
+//	    if (event.getEntity().posY < fogStartY)
+//	    {
+//		fogDensity += (float) (fogStartY - event.getEntity().posY) * (maxFog / fogStartY);
+//	    }
+//
+//	    event.setDensity(fogDensity);
+//	    event.setCanceled(true);
+//	}
     }
 }
