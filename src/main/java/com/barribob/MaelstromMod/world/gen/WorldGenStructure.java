457053445dcc903b6948ab03f71bb5ea32d92f47
miyo6032 <miyo6032@colorado.edu>
2019-09-14 07:14:54 -0600

    Add random rotations to structures

--- src/main/java/com/barribob/MaelstromMod/world/gen/WorldGenStructure.java
+++ src/main/java/com/barribob/MaelstromMod/world/gen/WorldGenStructure.java
@@ -5,12 +5,14 @@ import java.util.Map.Entry;
 import java.util.Random;
 
 import com.barribob.MaelstromMod.util.IStructure;
+import com.barribob.MaelstromMod.util.ModRandom;
 import com.barribob.MaelstromMod.util.Reference;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -60,9 +62,17 @@ public class WorldGenStructure extends WorldGenerator implements IStructure
 	Template template = manager.get(mcServer, location);
 	if (template != null)
 	{
-	    template.addBlocksToWorld(world, pos, settings.setRotation(rot), 18);
-	    
-	    Map<BlockPos, String> dataBlocks = template.getDataBlocks(pos, settings);
+	    Tuple<Rotation, BlockPos>[] rotations = new Tuple[] { new Tuple(Rotation.NONE, new BlockPos(0, 0, 0)),
+		    new Tuple(Rotation.CLOCKWISE_90, new BlockPos(template.getSize().getX() - 1, 0, 0)),
+		    new Tuple(Rotation.COUNTERCLOCKWISE_90, new BlockPos(0, 0, template.getSize().getZ() - 1)),
+		    new Tuple(Rotation.CLOCKWISE_180, new BlockPos(template.getSize().getX() - 1, 0, template.getSize().getZ() - 1)) };
+
+	    Tuple<Rotation, BlockPos> randomRotation = ModRandom.choice(rotations, world.rand);
+	    PlacementSettings rotatedSettings = settings.setRotation(randomRotation.getFirst());
+	    BlockPos rotatedPos = pos.add(randomRotation.getSecond());
+
+	    template.addBlocksToWorld(world, rotatedPos, rotatedSettings, 18);
+	    Map<BlockPos, String> dataBlocks = template.getDataBlocks(rotatedPos, rotatedSettings);
 	    for (Entry<BlockPos, String> entry : dataBlocks.entrySet())
 	    {
 		String s = entry.getValue();
@@ -76,7 +86,9 @@ public class WorldGenStructure extends WorldGenerator implements IStructure
     }
 
     /**
-     * Called when a data structure block is found, in order to replace it with something else
+     * Called when a data structure block is found, in order to replace it with
+     * something else
+     * 
      * @param function
      * @param pos
      * @param worldIn
