d59ea1214255b68938084634afa8b4fb2a08652f
miyo6032 <miyo6032@colorado.edu>
2020-01-31 11:57:42 -0700

    Remove junk blocks attached to dark nexus structure

--- src/main/java/com/barribob/MaelstromMod/entity/tileentity/TileEntityMegaStructure.java
+++ src/main/java/com/barribob/MaelstromMod/entity/tileentity/TileEntityMegaStructure.java
@@ -243,7 +243,7 @@ public class TileEntityMegaStructure extends TileEntityStructure
     @Override
     public void setSize(BlockPos sizeIn)
     {
-	this.size = new BlockPos(11, 85, 11);
+	this.size = new BlockPos(33, 32, 140);
     }
 
     @Override
@@ -535,7 +535,7 @@ public class TileEntityMegaStructure extends TileEntityStructure
             MinecraftServer minecraftserver = this.world.getMinecraftServer();
             TemplateManager templatemanager = worldserver.getStructureTemplateManager();
             Template template = templatemanager.getTemplate(minecraftserver, new ResourceLocation(this.name));
-	    template.takeBlocksFromWorld(this.world, blockpos, this.size, !this.ignoreEntities, Blocks.STRUCTURE_VOID);
+	    template.takeBlocksFromWorld(this.world, blockpos, this.size, !this.ignoreEntities, Blocks.AIR);
             template.setAuthor(this.author);
             return !writeToDisk || templatemanager.writeTemplate(minecraftserver, new ResourceLocation(this.name));
         }
