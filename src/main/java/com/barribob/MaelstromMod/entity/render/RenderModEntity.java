bd96ba28ef5d788cf8b3bfbf762070829ee9e3ea
Michael Yoshimura <miyo6032@colorado.edu>
2020-02-29 18:12:14 -0700

    First attempt at disintegration
    The standard dragon approach won't work, and the gL greater approach also won't check out, we we're going to try stencil buffers.

--- src/main/java/com/barribob/MaelstromMod/entity/render/RenderModEntity.java
+++ src/main/java/com/barribob/MaelstromMod/entity/render/RenderModEntity.java
@@ -1,7 +1,10 @@
 package com.barribob.MaelstromMod.entity.render;
 
+import org.lwjgl.opengl.GL11;
+
 import com.barribob.MaelstromMod.config.ModConfig;
 import com.barribob.MaelstromMod.entity.entities.EntityLeveledMob;
+import com.barribob.MaelstromMod.entity.entities.EntityMaelstromMob;
 import com.barribob.MaelstromMod.util.Element;
 import com.barribob.MaelstromMod.util.IElement;
 import com.barribob.MaelstromMod.util.Reference;
@@ -22,11 +25,11 @@ import net.minecraft.util.ResourceLocation;
 public class RenderModEntity<T extends EntityLiving> extends RenderLiving<T>
 {
     public String[] TEXTURES;
+    private ResourceLocation DEATH_TEXTURES;
 
     public <U extends ModelBase> RenderModEntity(RenderManager rendermanagerIn, String textures, U modelClass)
     {
-	super(rendermanagerIn, modelClass, 0.5f);
-	this.TEXTURES = new String[] { textures };
+	this(rendermanagerIn, modelClass, new String[] { textures });
     }
 
     public <U extends ModelBase> RenderModEntity(RenderManager rendermanagerIn, U modelClass, String... textures)
@@ -37,6 +40,7 @@ public class RenderModEntity<T extends EntityLiving> extends RenderLiving<T>
 	    throw new IllegalArgumentException("Must provide at least one texture to render an entity.");
 	}
 	this.TEXTURES = textures;
+	this.DEATH_TEXTURES = new ResourceLocation(String.format("%s:textures/entity/disintegration_%d_%d.png", Reference.MOD_ID, modelClass.textureWidth, modelClass.textureHeight));
     }
 
     @Override
@@ -64,12 +68,85 @@ public class RenderModEntity<T extends EntityLiving> extends RenderLiving<T>
     }
 
     @Override
+    protected void applyRotations(T entityLiving, float p_77043_2_, float rotationYaw, float partialTicks)
+    {
+	if (entityLiving instanceof EntityMaelstromMob)
+	{
+	    GlStateManager.rotate(180.0F - rotationYaw, 0.0F, 1.0F, 0.0F);
+	}
+	else
+	{
+	    super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
+	}
+    }
+
+    @Override
+    protected void renderModel(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor)
+    {
+	if (entity instanceof EntityMaelstromMob)
+	{
+	    if (entity.getHealth() <= 0)
+	    {
+		float f = entity.deathTime / (15f); // The alpha value required to render a pixel (multiplied by 95 because the actual image was erased by 95)
+//		GlStateManager.depthFunc(GL11.GL_LEQUAL);
+//		GlStateManager.enableAlpha();
+//		GlStateManager.alphaFunc(GL11.GL_GREATER, f);
+		this.bindTexture(DEATH_TEXTURES);
+		
+		/**
+		 * We've gotten somewhere with this problem. The only thing that really doesn't work here is the GL GREATER which will render mobs through walls
+		 * try this masking somehow: https://stackoverflow.com/questions/5097145/opengl-mask-with-multiple-textures
+		 * Actually maybe use the stencil buffer or something like it: https://stackoverflow.com/questions/292071/opengl-how-to-implement-an-eraser-tool
+		 * I believe we can do this with stencil buffering hopefully.
+		 */
+		GlStateManager.colorMask(false, false, false, false);
+		GlStateManager.depthMask(false);
+		GL11.glEnable(GL11.GL_STENCIL_TEST);
+		GL11.glStencilFunc(GL11.GL_ALWAYS, 1, 0xFFFFFFFF);
+		GL11.glStencilOp(GL11.GL_REPLACE, GL11.GL_REPLACE, GL11.GL_REPLACE);
+
+		// Rendering similar to the creeper aura thingy
+//		GlStateManager.matrixMode(5890);
+//		GlStateManager.loadIdentity();
+//		GlStateManager.matrixMode(5888);
+//		GlStateManager.enableBlend();
+//		GlStateManager.color(0.1f, 0.2f, 0.5f, 0.5F); // Color it purple
+//		GlStateManager.disableLighting();
+//		GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+//		GlStateManager.scale(1.01, 1.01, 1.01);
+		this.mainModel.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+//		GlStateManager.matrixMode(5890);
+//		GlStateManager.loadIdentity();
+//		GlStateManager.matrixMode(5888);
+//		GlStateManager.enableLighting();
+//		GlStateManager.disableBlend();
+		
+		GL11.glStencilFunc(GL11.GL_EQUAL, 0, 0xFFFFFFFF);
+		GL11.glStencilOp(GL11.GL_KEEP, GL11.GL_KEEP, GL11.GL_KEEP);
+		GlStateManager.colorMask(true, true, true, true);
+		GlStateManager.depthMask(true);
+
+//		GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+//		GlStateManager.depthFunc(GL11.GL_GREATER); // This makes it so that only the stuff that's rendered before gets rendered in the real render below
+	    }
+
+	    this.bindEntityTexture(entity);
+	    this.mainModel.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+
+	    GlStateManager.depthFunc(GL11.GL_LEQUAL);
+	}
+	else
+	{
+	    super.renderModel(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor);
+	}
+    }
+
+    @Override
     public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks)
     {
 	if (!entity.isInvisible())
 	{
 	    // The blending here allows for rendering of translucent textures
-	    GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 	    GlStateManager.enableNormalize();
 	    GlStateManager.enableBlend();
 	    GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
