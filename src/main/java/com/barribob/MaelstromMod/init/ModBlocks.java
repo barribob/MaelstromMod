13e9bf15fb3f586372d25b00258170a799c3acfe
Michael Yoshimura <miyo6032@colorado.edu>
2020-04-04 22:58:37 -0600

    Added a bunch of new blocks and retextured crimson lamp

--- src/main/java/com/barribob/MaelstromMod/init/ModBlocks.java
+++ src/main/java/com/barribob/MaelstromMod/init/ModBlocks.java
@@ -31,6 +31,7 @@ import com.barribob.MaelstromMod.blocks.BlockNexusTeleporter;
 import com.barribob.MaelstromMod.blocks.BlockPillarBase;
 import com.barribob.MaelstromMod.blocks.BlockPlumFilledLeaves;
 import com.barribob.MaelstromMod.blocks.BlockPlumLeaves;
+import com.barribob.MaelstromMod.blocks.BlockRedstoneBrick;
 import com.barribob.MaelstromMod.blocks.BlockSaplingBase;
 import com.barribob.MaelstromMod.blocks.BlockStairsBase;
 import com.barribob.MaelstromMod.blocks.BlockSwampLeaves;
@@ -185,7 +186,7 @@ public class ModBlocks
     public static final Block CRIMSON_MAELSTROM_BRICK_FENCE = new BlockFenceBase("crimson_maelstrom_brick_fence", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
     public static final Block CRIMSON_MAELSTROM_BRICK_STAIRS =
 	    new BlockStairsBase("crimson_maelstrom_brick_stairs", CRIMSON_MAELSTROM_BRICKS.getDefaultState(), BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
-    public static final Block CRIMSON_LAMP = new BlockLamp("crimson_lamp", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE)
+    public static final Block CRIMSON_LAMP = new BlockLamp("crimson_lamp", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE, true)
     {
 	@Override
 	@SideOnly(Side.CLIENT)
@@ -197,6 +198,10 @@ public class ModBlocks
     public static final Block CHAIN = new BlockChain("chain", Material.IRON, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.METAL, new AxisAlignedBB(0.425D, 0.0D, 0.425D, 0.575D, 1.0D, 0.575D)).setCreativeTab(ModCreativeTabs.BLOCKS);
     public static final Block MULTI_CHAIN = new BlockChain("multi_chain", Material.IRON, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.METAL, new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 1.0D, 0.75D)).setCreativeTab(ModCreativeTabs.BLOCKS);
     public static final Block LARGE_CHAIN = new BlockChain("large_chain", Material.IRON, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.METAL, new AxisAlignedBB(0.30D, 0.0D, 0.3D, 0.7D, 1.0D, 0.7D)).setCreativeTab(ModCreativeTabs.BLOCKS);
+    public static final Block METAL_LAMP = new BlockLamp("metal_lamp", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE, true).setLightLevel(1.0f).setCreativeTab(ModCreativeTabs.BLOCKS);
+    public static final Block FURNACE_BRICKS = new BlockBase("furnace_bricks", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
+    public static final Block FURNACE_STAIRS = new BlockStairsBase("furnace_stairs", FURNACE_BRICKS.getDefaultState(), BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
+    public static final Block CRACKED_FURNACE_BRICKS = new BlockBase("cracked_furnace_bricks", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
 
     /*
      * Key blocks and nexus stuff
@@ -216,21 +221,8 @@ public class ModBlocks
      */
 
     public static final Block FURNACE_PILLAR = new BlockPillarBase("furnace_pillar", Material.ROCK).setHardness(BRICK_HARDNESS).setResistance(STONE_RESISTANCE).setCreativeTab(ModCreativeTabs.BLOCKS);
-    public static final Block REDSTONE_BRICK = new BlockBase("redstone_brick", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE)
-    {
-	@Override
-	public boolean canProvidePower(net.minecraft.block.state.IBlockState state)
-	{
-	    return true;
-	};
-
-	@Override
-	public int getWeakPower(net.minecraft.block.state.IBlockState blockState, net.minecraft.world.IBlockAccess blockAccess, net.minecraft.util.math.BlockPos pos,
-		net.minecraft.util.EnumFacing side)
-	{
-	    return 15;
-	};
-    }.setCreativeTab(ModCreativeTabs.BLOCKS);
+    public static final Block REDSTONE_BRICK = new BlockRedstoneBrick("redstone_brick", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
+    public static final Block CRACKED_REDSTONE_BRICK = new BlockRedstoneBrick("cracked_redstone_brick", Material.ROCK, BRICK_HARDNESS, STONE_RESISTANCE, SoundType.STONE).setCreativeTab(ModCreativeTabs.BLOCKS);
     public static final Block IRON_GRATE = new BlockGrate("iron_grate", Material.IRON).setHardness(BRICK_HARDNESS).setResistance(STONE_RESISTANCE).setCreativeTab(ModCreativeTabs.BLOCKS);
     public static final Block FAN = new BlockFan("fan", Material.IRON, STONE_HARDNESS, STONE_RESISTANCE, SoundType.METAL);
 
