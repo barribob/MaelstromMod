946d27c6c6a645a29b096a3d2e322fe9a0ba424a
Michael Yoshimura <miyo6032@colorado.edu>
2020-07-15 20:25:52 -0600

    Alter entity kill messages

--- src/main/java/com/barribob/MaelstromMod/util/ModDamageSource.java
+++ src/main/java/com/barribob/MaelstromMod/util/ModDamageSource.java
@@ -14,24 +14,12 @@ public class ModDamageSource {
     private static final String MAELSTROM = Reference.MOD_ID + ":" + "maelstrom";
     private static final String MOB_MAELSTROM = Reference.MOD_ID + ":" + "mobMaelstrom";
     private static final String PLAYER_MAELSTROM = Reference.MOD_ID + ":" + "playerMaelstrom";
-    private static final String ARROW_MAELSTROM = Reference.MOD_ID + ":" + "arrowMaelstrom";
     private static final String THROWN_MAELSTROM = Reference.MOD_ID + ":" + "thrownMaelstrom";
     private static final String EXPLOSION_MAELSTROM = Reference.MOD_ID + ":" + "explosionMaelstrom";
     private static final String EXPLOSION_MAELSTROM_ENTITY = Reference.MOD_ID + ":" + "explosionMaelstrom.player";
     private static final String MAGIC_MAELSTROM = Reference.MOD_ID + ":" + "magicMaelstrom";
 
     public static final DamageSource MAELSTROM_DAMAGE = (new DamageSource(MAELSTROM));
-
-    /**
-     * Return whether a certain damage type is maelstrom damage
-     *
-     * @param source
-     * @return
-     */
-    public static boolean isMaelstromDamage(DamageSource source) {
-        return source.damageType == MOB_MAELSTROM || source.damageType == PLAYER_MAELSTROM || source.damageType == ARROW_MAELSTROM || source.damageType == THROWN_MAELSTROM
-                || source.damageType == EXPLOSION_MAELSTROM || source.damageType == EXPLOSION_MAELSTROM_ENTITY || source.damageType == MAELSTROM;
-    }
 
     public static DamageSource causeElementalMeleeDamage(EntityLivingBase mob, Element element) {
         return new EntityElementalDamageSource(MOB_MAELSTROM, mob, element);
