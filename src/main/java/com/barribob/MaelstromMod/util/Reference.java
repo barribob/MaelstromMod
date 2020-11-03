f785867f330f5c3edeb4cb67b29b6310d008bf3e
Michael Yoshimura <miyo6032@colorado.edu>
2020-11-02 21:06:57 -0700

    Version bump to snapshot 6

--- src/main/java/com/barribob/MaelstromMod/util/Reference.java
+++ src/main/java/com/barribob/MaelstromMod/util/Reference.java
@@ -6,7 +6,7 @@ package com.barribob.MaelstromMod.util;
 public class Reference {
     public static final String MOD_ID = "mm";
     public static final String NAME = "Maelstrom Mod";
-    public static final String VERSION = "0.4.1-SNAPSHOT.5";
+    public static final String VERSION = "0.4.1-SNAPSHOT.6";
     public static final String ACCEPTED_VERSIONS = "[1.12]";
     public static final String CLIENT_PROXY_CLASS = "com.barribob.MaelstromMod.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.barribob.MaelstromMod.proxy.CommonProxy";
